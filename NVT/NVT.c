b278582c2fa6c90a7c0bc29a7c322151fbe55e41
Luciano <luciano.e.laratelli@outlook.com>
2016-09-19 21:47:56 -0400

    some actual work
    Finished describing individual functions; wrote code for distfinder,
    PEfinder, and sketched out the main body of code
    also wrote a to-do-list!

--- NVT/NVT.c
+++ NVT/NVT.c
@@ -1,6 +1,7 @@
 #include <stdio.H>
 #include <math.H>
 #include <stdlib.H>
+#include <stdbool.H>
 
 struct particle
 {
@@ -11,17 +12,37 @@ double sigma, epsilon, m;
 
 #define N 5 //number of particles
 struct particle particles[N];
+/******************************************************************************
+TO DO:
+1. Figure out how starting_positions will work
+2. Translate the math for E_checker into code
+3. Work out how much the random displacement should be for rand_p_mover
+4. Write code for output to file
+5. ???
+6. profit
+******************************************************************************/
+
+
 
 /************************
 distfinder finds the distance between any two distinct particles of
 number "id_a" and "id_b" respectively. this distance is used to
-calculate the potential energy in PEfinder using the lennard-jones
-potential, so it's important
+calculate the potential energy in PEfinder using the lennard-jones potential 
 ************************/
 double distfinder(int id_a, int id_b)
 {
-
-    return dist
+    int i;
+    double dist, dist2;
+    dist2 = 0;//clearing junk from memory
+    for(i=0;i<3;i++)
+    {
+        dist2 += pow(particles[id_a].x[i]-particles[id_b].x[i],2);
+        //finds distance coordinate by coordinate,
+        //keeping the sum of the squares in memory
+    }
+    dist = sqrt(dist2); 
+    //distance formula says dist = sqrt of the sum of the squares
+    return dist;
 }
 
 /**********************
@@ -32,13 +53,47 @@ can be changed if you need a better model
 **********************/
 double PEfinder()
 {
-
+    double sigma, epsilon, r;
+    double s2,s4,s6,s12;//exponents of sigma
+    double rinv,r2,r4,r6,r12;//exponents of r
+    double sor6,sor12;
+    epsilon = 1; //this can be changed to experimental values
+    sigma = 1; / same as above
+    s2 = sigma * sigma;
+    s6 = s2*s2*s2;
+    s12 = s6*s6;
+    for(b=0;b<N;b++)
+    {
+        for(c=0;c<N;c++)
+        {
+            if(b!=c)
+            {
+                r = distfinder(b,c);
+                rinv = 1/r;
+                //formula has sigma/r; this is a bit easier to work with
+                r2 = rinv * rinv;
+                r6 = r2*r2*r2;
+                r12 = r6* r6;
+                sor6 = r6 * s6;
+                sor12 = r12 * s12;
+                //makes it harder to mess up the formula, h/t adam for the tip
+                for(i=0;i<3;i++)
+                {
+                    pe += 0.5 * (4 * epsilon * ((sor12)-sor6));
+                    // each sum does half the PE per pair
+                    // because the way this loop is written 
+                    // has each pair included twice
+                    // typing a "0.5 *" is easier than thinking about loops
+                }
+            }
+        }
+    }           
     return pe;
 }
 
 /*********************
 this next function is part of the monte carlo method; it moves a random 
-particle a random distance away
+particle a random distance away from its current position
 *********************/
 void rand_p_mover()
 {
@@ -60,13 +115,54 @@ bool E_checker()
     return False;
 }
 
-void position_receiver()
+/*******************
+starting_positions takes a .txt file with coordinates 
+for the starting positions of the atoms in this simulation.
+The first line of the file is the x coordinate of particle p
+the second line of the file is the y coordinate of particle p
+the third line of the file is the z coordinate of particle p
+etc.
+*******************/
+void starting_positions()
 {
-
+for(p=0;p<N;p++)
+{
+    for(i=0;i<3;i++)
+    {
+        particles[p].x[i] = //how to read line p from file?
+            //= p * (i + 1) 
     return;
 }
-
+/******************
+output_to_file lives up to its name. It ouputs two things:
+the coordinates of the particles at every frame into an .xyz file;
+the total energy of the system at every frame
+*******************/
+void output_to_file()
+{
+    return;
+}
 int main()
 {
+    FILE * positions;//this is the .xyz file 
+    FILE * energies; //this is the .txt file
+    bool guess; 
+    int c;//count
+    int m;//maximum number of tries
+    starting_positions();
+    for(c=0;c<m;c++)
+    {
+        pe = PEfinder();
+        rand_p_mover();
+        guess = E_checker();
+        if (guess) == 1)
+        {
+            output_to_file;
+        }
+        else
+        {
+            continue;
+        }
+    }
     return 0;
 }
